e6883cdb87128c5dd882ec888119df9ccdc46a75
Antony <akb@akb.me.uk>
2018-02-18 14:32:30 +0000

    Fetched files were incorrectly written.  Pipe the response to the file.

--- fetch_lean_js.ts
+++ fetch_lean_js.ts
@@ -11,10 +11,12 @@ const baseUrl = 'https://leanprover.github.io/live/latest/';
 
 ['lean_js_js.js', 'lean_js_wasm.js', 'lean_js_wasm.wasm', 'library.zip'].forEach((filename: string) => {
     superagent.get(baseUrl + filename).then((response: superagent.Response) => {
-        fs.writeFile(dir + filename, response.body, (err: NodeJS.ErrnoException) => {
+
+        fs.open(dir + filename, 'w', (err, fileDescriptor) => {
             if (err) {
                 console.error('Error: ' + err.message);
             } else {
+                response.pipe(fs.createWriteStream(null, {fd: fileDescriptor}));
                 console.log(filename);
             }
         });

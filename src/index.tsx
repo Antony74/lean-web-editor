65e4d186df6b0643a414bed67fcdf010a6b3d2c0
Antony <akb@akb.me.uk>
2018-03-19 20:37:13 +0000

    A page containing exercises does not persist the contents of the editor in the url.

--- src/index.tsx
+++ src/index.tsx
@@ -5,27 +5,14 @@ import { render } from 'react-dom';
 import { registerLeanLanguage } from './langservice';
 import { LeanEditor } from './LeanEditor';
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
-function fixedEncodeURIComponent(str) {
-  return encodeURIComponent(str).replace(/[!'()*]/g, (c) => {
-    return '%' + c.charCodeAt(0).toString(16);
-  });
-}
-
 const defaultValue =
   '-- Live javascript version of Lean\n\nexample (m n : ℕ) : m + n = n + m :=\nby simp';
 
 function App() {
-  let value = defaultValue;
-  if (window.location.hash.startsWith('#code=')) {
-    value = decodeURIComponent(window.location.hash.substring(6));
-  }
 
   const fn = monaco.Uri.file('test.lean').fsPath;
   return (
-    <LeanEditor file={fn} initialValue={value} onValueChange={(newValue) => {
-      history.replaceState(undefined, undefined, '#code=' + fixedEncodeURIComponent(newValue));
-    }} />
+    <LeanEditor file={fn} initialValue={defaultValue} />
   );
 }
 

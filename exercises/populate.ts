85cefd5b741d4daf17d979056d56f390df5911e5
Antony <akb@akb.me.uk>
2018-03-25 16:48:37 +0100

    Export each exercise as html.

--- exercises/populate.ts
+++ exercises/populate.ts
@@ -19,6 +19,67 @@ const yamlString = fs.readFileSync(__dirname + '/sessions.yaml', 'utf-8');
 const allSessions: Session[] = safeLoad(yamlString);
 const sessions = allSessions.slice(0, 5); // Just take the sessions on proposistional logic
 
-sessions.forEach((session: Session) => {
-    console.log([session.name, 'contains', session.tasks.length, 'tasks'].join(' '));
+const tsxLines: string[] = [];
+
+tsxLines.push('export class Exercise {');
+tsxLines.push('    html: JSX.Element;');
+tsxLines.push('    code: string;');
+tsxLines.push('  }');
+tsxLines.push('');
+
+tsxLines.push('export const exercises: Exercise[][] = [');
+
+sessions.forEach((session: Session, sessionIndex: number) => {
+
+    tsxLines.push('  [');
+
+    session.tasks.forEach((task: Task, taskIndex: number) => {
+        tsxLines.push('    {');
+        tsxLines.push('      html:');
+        tsxLines.push(getHtml(task, sessionIndex, taskIndex) + ',');
+        tsxLines.push('      code: \'\',');
+        tsxLines.push('    },');
+    });
+
+    tsxLines.push('  ],');
 });
+
+tsxLines.push('];');
+tsxLines.push('');
+
+fs.writeFileSync(__dirname + '/../exercises.tsx', tsxLines.join('\n'), 'utf8');
+
+function getHtml(task: Task, sessionIndex: number, taskIndex: number) {
+    const htmlLines: string[] = [];
+
+    htmlLines.push('<div>');
+    const exerciseNumber = [sessionIndex + 1, taskIndex + 1].join('.');
+    htmlLines.push('  Excercise ' + exerciseNumber + '<br/>');
+    console.log(exerciseNumber);
+
+    if (task.assumptions && task.assumptions.length) {
+        htmlLines.push('  Given');
+        htmlLines.push('  <ul>');
+
+        task.assumptions.forEach((item: string) => {
+            htmlLines.push('    <li>' + item + '</li>');
+        });
+
+        htmlLines.push('  </ul>');
+    }
+
+    htmlLines.push('  Show');
+    htmlLines.push('  <ul>');
+
+    task.conclusions.forEach((item: string) => {
+        htmlLines.push('    <li>' + item + '</li>');
+    });
+
+    htmlLines.push('  </ul>');
+
+    htmlLines.push('</div>');
+
+    return htmlLines.map((s: string): string => {
+        return '        ' + s;
+    }).join('\n');
+}

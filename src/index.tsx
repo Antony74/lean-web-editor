25bbacca3f93694fcc39ec57e72cf7fa95e7c039
Antony <akb@akb.me.uk>
2018-03-30 18:19:58 +0100

    Get the pagination working (apart from the contents of the Lean Editors, and a desire to start any new page at the top).

--- src/index.tsx
+++ src/index.tsx
@@ -18,14 +18,32 @@ class Pagination extends React.Component<PaginationProps> {
   }
 
   render() {
+
+    let prevClass = 'page-item';
+    let nextClass = 'page-item';
+
+    if (this.props.nSession < 1) {
+      prevClass += ' disabled';
+    }
+
+    if (this.props.nSession + 1 >= sessions.length) {
+      nextClass += ' disabled';
+    }
+
     return (
       <nav aria-label='Session pages'>
         <ul className='pagination justify-content-center'>
-          <li className='page-item'><a className='page-link' href='#'>Previous</a></li>
-          <li className='page-item'><a className='page-link' href='#'>1</a></li>
-          <li className='page-item'><a className='page-link' href='#'>2</a></li>
-          <li className='page-item'><a className='page-link' href='#'>3</a></li>
-          <li className='page-item'><a className='page-link' href='#'>Next</a></li>
+          <li className={prevClass}><a className='page-link' href={'#/' + this.props.nSession}>Previous</a></li>
+          {
+            sessions.map((session: Session, index: number) => {
+              const nSession = index + 1;
+
+              return (
+                <li className='page-item'><a className='page-link' href={'#/' + nSession}>{nSession}</a></li>
+              );
+            })
+          }
+          <li className={nextClass}><a className='page-link' href={'#/' + (this.props.nSession + 2)}>Next</a></li>
         </ul>
       </nav>
     );
@@ -75,6 +93,7 @@ class TheSession extends React.Component<RouteComponentProps<TheSessionRouteProp
             );
           })
         }
+        <Pagination nSession={nSession} />
       </div>
     );
   }
